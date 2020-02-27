a73b7bd660fbb353c5b9a468cea2ce24505e255a
jams10 <jamin3710@gmail.com>
2020-02-27 21:28:23 +0900

    Added Interior/Tile mode.

---
+++ Unity Project File/Assets/InteriorButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+namespace GoogleARCore.Examples.ObjectManipulation
+{
+    public class InteriorButton : MonoBehaviour
+    {
+        public void onClickedButton()
+        {
+            gameObject.GetComponent<PawnManipulator>().enabled = true;
+            GameObject.FindWithTag("TileMesh").SetActive(false);
+        }
+    }
+}
