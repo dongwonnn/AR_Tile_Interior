0d5501e5f59881f75771fc70c96bbd479c10f190
Jamin <jamin3710@gmail.com>
2020-04-19 11:34:36 +0900

    Added AssetBundle Manager and Texture Change Function

---
+++ Unity Project File/Assets/TextureChangeButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureChangeButton : MonoBehaviour
+{
+    public GameObject obj;
+    private TextureChangeTest textureChange;
+    private PolygonTesterZ polygonTester;
+    int n = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textureChange = obj.GetComponent<TextureChangeTest>();
+        polygonTester = obj.GetComponent<PolygonTesterZ>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClickedButton()
+    {
+        polygonTester.isCreated = true;
+        if (n < textureChange.mats.Count)
+        {
+            GetComponent<MeshRenderer>().material = textureChange.mats[n];
+            n++;
+        }
+    }
+}
---
+++ Unity Project File/Assets/TextureChangeTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class TextureChangeTest : MonoBehaviour
+{
+    public List<Material> mats = new List<Material>();
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadAssetBundle());
+    }
+
+    IEnumerator LoadAssetBundle()
+    {
+        string uri = "file:///" + Application.dataPath + "/AssetBundles/tilemaps0";
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(uri);
+
+        yield return request.SendWebRequest();
+
+        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+
+ 
+        for(int i=0; i<2; i++)
+        {
+            var mat = bundle.LoadAsset<Material>("tilemat" + i);
+            mats.Add(mat);
+        }
+        
+        //GetComponent<MeshRenderer>().material = mats[0];
+
+        Debug.Log(mats.Count);
+        //var prefab = bundle.LoadAsset<GameObject>("Stone");
+        //Instantiate(prefab);
+
+        //prefab = bundle.LoadAsset<GameObject>("Brick");
+        //Instantiate(prefab);
+        
+        bundle.Unload(false);
+    }
+}
+
