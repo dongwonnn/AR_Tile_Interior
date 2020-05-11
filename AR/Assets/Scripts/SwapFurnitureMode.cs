b0aa5c59ffe8f3ea98362128de95e1c7b447473f
jams10 <jamin3710@gmail.com>
2020-05-11 21:17:43 +0900

    Added AR project

---
+++ AR/Assets/Scripts/SwapFurnitureMode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace GoogleARCore.Examples.ObjectManipulation
+{
+    public class SwapFurnitureMode : MonoBehaviour
+    {
+        GameObject pawnGenerator;
+        public Text furnitureModeText;
+        // Start is called before the first frame update
+        void Start()
+        {
+            pawnGenerator = GameObject.Find("Generator").transform.Find("PawnGenerator").gameObject;
+        }
+
+        // Update is called once per frame
+        public void onClickedButton()
+        {
+            if (pawnGenerator.GetComponent<PawnGenerator>().isFurnitureMode)
+            {
+                pawnGenerator.GetComponent<PawnGenerator>().isFurnitureMode = false;
+                furnitureModeText.text = "Furniture Mode OFF";
+            }
+            else
+            {
+                pawnGenerator.GetComponent<PawnGenerator>().isFurnitureMode = true;
+                furnitureModeText.text = "Furniture Mode ON";
+            }
+        }
+    }
+}
---
+++ AR/Assets/Scripts/UIShow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIShow : MonoBehaviour
+{
+    public GameObject CreateButton;
+    public GameObject TextureButton;
+    public GameObject PlaneButton;
+    public GameObject ScrollView;
+    public GameObject FurnitureModeButton;
+    public GameObject FurnitureModeText;
+
+    //GameObject MeshGenerator;
+    GameObject PawnGenerator;
+    //GameObject ManipulationSystem;
+
+    bool isOnScrollView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CreateButton = GameObject.Find("CreateButton");
+        //TextureButton = GameObject.Find("Canvas").transform.Find("TextureChangeButton").gameObject;
+        //PlaneButton = GameObject.Find("Canvas").transform.Find("HidePlaneButton").gameObject;
+        //ScrollView = GameObject.Find("Canvas").transform.Find("TextureScrollView").gameObject;
+        //TextureRotationButton = GameObject.Find("Canvas").transform.Find("TextureRotationButton").gameObject;
+        //FurnitureModeButton = GameObject.Find("Canvas").transform.Find("FurnitureModeButton").gameObject;
+        //FurnitureModeText = GameObject.Find("Canvas").transform.Find("FurnitureModeText").gameObject;
+
+        PawnGenerator = GameObject.Find("Generator").transform.Find("PawnGenerator").gameObject;
+        
+        isOnScrollView = false;
+    }
+
+    public void afterCreation()
+    {
+        CreateButton.SetActive(false);
+        TextureButton.SetActive(true);
+        PlaneButton.SetActive(true);
+        PawnGenerator.SetActive(true);
+        FurnitureModeButton.SetActive(true);
+        FurnitureModeText.SetActive(true);
+    }
+
+    public void scrollView()
+    {
+        if(!isOnScrollView)
+        {
+            ScrollView.SetActive(true);
+            isOnScrollView = true;
+        }
+        else
+        {
+            ScrollView.SetActive(false);
+            isOnScrollView = false;
+        }
+    }
+}
